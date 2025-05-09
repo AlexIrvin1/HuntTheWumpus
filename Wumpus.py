653ede3e3eaa8135ded12cd826f82a91c6ab52ba
AlexIrvin1 <griffinalex2003@hotmail.com>
2025-05-09 15:43:21 -0400

    Added Player Movement and Hazard Checks!

--- Wumpus.py
+++ Wumpus.py
@@ -19,20 +19,19 @@ def create_cave_system():
     # Create all rooms
     rooms = {i: Room(i) for i in range(1, 21)}
 
-    # Manually connect the rooms to match the dodecahedron structure
     connections = {
-        1: [2, 5, 6],
-        2: [1, 3, 8],
-        3: [2, 4, 10],
-        4: [3, 5, 12],
-        5: [1, 4, 7],
-        6: [1, 7, 11],
-        7: [5, 6, 8],
-        8: [2, 7, 9],
-        9: [8, 10, 12],
-        10: [3, 9, 11],
-        11: [6, 10, 12],
-        12: [4, 9, 11]
+        1: [3, 8, 11],
+        2: [4, 7, 12],
+        3: [1, 6, 10],
+        4: [2, 5, 9],
+        5: [4, 6, 11],
+        6: [3, 5, 12],
+        7: [2, 8, 10],
+        8: [1, 7, 9],
+        9: [4, 8, 12],
+        10: [3, 7, 11],
+        11: [1, 5, 10],
+        12: [2, 6, 9]
     }
 
     for room, neighbors in connections.items():
@@ -55,3 +54,24 @@ def place_hazards(rooms):
     wumpus_room.has_wumpus = True
     return wumpus_room
 
+class Player:
+    def __init__(self, start_room):
+        self.current_room = start_room
+
+    def move(self, target_room):
+        if target_room in self.current_room.tunnels:
+            self.current_room = target_room
+        else:
+            print("You can't move that way!")
+
+def checkHazards(player):
+    if player.current_room.has_pit:
+        print("You fell into a bottomless pit! GG's Game Over!")
+        return False
+    elif player.current_room.has_bats:
+        print("Giant bats whisk you away to a random room!")
+        player.current_room = random.choice(list(player.current_room.tunnels))
+    elif player.current_room.has_wumpus:
+        print("You stumbled into the Wumpus's lair! Game Over!")
+        return False
+    return True
